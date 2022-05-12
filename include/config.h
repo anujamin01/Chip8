c226a7bed7f28f525fd0c0c4c97bce32feb83683
Anuj <anujamin-1@gmail.com>
2022-05-12 16:40:20 -0500

    Updated Variable Names

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define EMULATOR_WINDOW_TITLE "Chip8 emu"
+#define CHIP8_MEMORY_SIZE 4096
+#define CHIP_8_WIDTH 64
+#define CHIP_8_HEIGHT 32
+#define WINDOW_RESIZER 10 
+
+#endif
