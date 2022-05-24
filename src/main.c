5d33f66972582d7eabe5b35e089563281cba7681
Anuj <anujamin-1@gmail.com>
2022-05-23 22:30:23 -0500

    Implemented character set

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@ const char keyboard_map[CHIP8_TOTAL_KEYS] = {
 int main(int argc, char** argv){
 
   struct chip8 chip8;
+  chip8_init(&chip8);
   //chip8_keyboard_down(&chip8.keyboard, 0xf);
   //chip8_keyboard_up(&chip8.keyboard, 0xf);
   //bool is_down = chip8_keyboard_is_down(&chip8.keyboard, 0xf);
